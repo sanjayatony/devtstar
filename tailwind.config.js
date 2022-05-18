f439f83bde1b838546c94b876ba3e9634a70821f
Tony Sanjaya <tony@vegapunk.local>
2022-05-18 16:10:14 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js,njk}"],
   theme: {
-    extend: {
-      fontFamily: {
-        'blex': ['IBM Plex Mono', 'monospace'],
-      }
-    },
+    extend: {},
+    fontFamily: {
+      'blex': ['IBM Plex Mono', 'monospace'],
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
